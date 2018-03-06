1dea6e49389b932a3adaf0ca40003046c9e8c699
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2018-03-05 20:31:58 -0600

    added media queries

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -3,13 +3,15 @@ import "./ResultCard.css";
 
 export default function ResultCard(props) {
     return (
-            <div className="card">
-                <h5 className="card-header">{props.name}</h5>
+        <div className="card">
+            <div className="card-img">
                 <img src={props.img} alt={props.name} className="result-img" />
-                <div className="result-description">
-                    <p>{props.price} <span className="divider">|</span> {props.distance} mile(s) away </p>
-                    <p><a href={props.yelp} target="_blank">Yelp</a><span className="divider"> | </span><a href={`https://www.google.com/maps/search/?api=1&query=${props.lat},${props.long}`} target="_blank">Open in Maps</a></p>
-                </div>
             </div>
+            <h5 className="card-header">{props.name}</h5>
+            <div className="result-description">
+                <p>{props.price} <span className="divider">|</span> {props.distance} mile(s) away </p>
+                <p><a href={props.yelp} target="_blank">Yelp</a><span className="divider"> | </span><a href={`https://www.google.com/maps/search/?api=1&query=${props.lat},${props.long}`} target="_blank">Open in Maps</a></p>
+            </div>
+        </div>
     )
 }
